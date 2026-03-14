103c7321c35e7dd970165664dd794068361a7501
OrjOsc <orjosc2005@gmail.com>
2026-03-14 16:35:31 +0100

    Revert "Update Player.cs"
    This reverts commit aa6ad42430e7ce3e3c849e183de73d0404410638.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 10f;
-    public Transform groundCheck;
-    public float groundCheckRadius = 0.2f;
-    public LayerMask groundLayer;
 
     private Rigidbody2D rb;
-    private bool isGrounded;
 
     void Start()
     {
@@ -21,15 +17,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
         float moveInput = Input.GetAxis("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if(Input.GetKeyDown(KeyCode.Space))
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
         }
 
-    }
-
-    private void FixedUpdate()
-    {
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
     }
 }
